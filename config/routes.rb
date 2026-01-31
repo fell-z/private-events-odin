ddcbe00fe3d18b4917f82797d47d8ac70834b04c
fell-z <matheusfelipeagapito@hotmail.com>
2026-01-31 20:47:40 -0300

    Add association between creator user and created event

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "events#index"
+
   devise_for :users
   get "events/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_31_204717) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_31_211322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.bigint "creator_id", null: false
     t.datetime "date"
     t.string "location"
     t.string "name"
     t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_31_204717) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
