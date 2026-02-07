23d1227b7a453f093cd0236735c2389d9b53b852
fell-z <matheusfelipeagapito@hotmail.com>
2026-02-07 17:07:13 -0300

    Add class methods for past/upcoming events
    - Made event's index page show past and upcoming events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances
+
+  def self.past
+    where("date < ?", Time.now.midnight)
+  end
+
+  def self.upcoming
+    where("date >= ?", Time.now.midnight)
+  end
 end
